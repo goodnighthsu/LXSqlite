5c5c352c9467a02c399315eaa37b3506f5b5f317
goodnighthsu <goodnighthsu@msn.com>
2013-02-20 00:42:53 +0800

    ver 0.0.1

---
+++ LXSqlite/LXSqlite/LXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LXAppDelegate.h
+//  LXSqlite
+//
+//  Created by Leon on 13-2-20.
+//  Copyright (c) 2013年 LeonXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LXSqlite/LXSqlite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LXSqlite
+//
+//  Created by Leon on 13-2-20.
+//  Copyright (c) 2013年 LeonXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LXAppDelegate class]));
+    }
+}
